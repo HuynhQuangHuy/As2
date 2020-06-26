1f9c99061a1ae43d59f572cf58386c5b5134739c
Asus <Asus@LAPTOP-QE1EIOQN>
2020-06-26 19:32:25 +0700

    commit

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -1,5 +1,19 @@
 <!DOCTYPE html>
 <html>
+    <style>
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: red;
+}
+</style>
 <body>
 
 <h1>DATABASE CONNECTION</h1>
