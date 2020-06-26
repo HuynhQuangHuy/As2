ec287585426f30a2f95469d4c070e7663c596e82
Asus <Asus@LAPTOP-QE1EIOQN>
2020-06-26 16:11:34 +0700

    phuoc commit

---
+++ DeleteCustomer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Delete customer information</h1>
+
+<form name="delete" method="POST" action="DeleteData.php">
+    <label for='customer'> Customerid</label><input type="text" name="customerid" placeholder="......."/><br>
+    <input type="submit" values="Delete">
+</form>
+<?php
+ini_set('display_errors', 1);
+echo "Insert database!";
+?>
+
+<?php
+
+
+if (empty(getenv("DATABASE_URL"))){
+    echo '<p>The DB does not exist</p>';
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+}  else {
+     
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+        "host=ec2-52-7-39-178.compute-1.amazonaws.com;port=5432;user=klkiodyeeoozkr;password=ae94a2076f4648fe1aa94b50d82109d7fb2eab5ff73bd21cecfe34f6fb217a1c;dbname=d4ncpltpdbuol8",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
+
+$sql = "DELETE FROM customer WHERE customerid = '$_POST[customerid]'";
+$stmt = $pdo->prepare($sql);
+if($stmt->execute() == TRUE){
+    echo "Record deleted successfully.";
+} else {
+    echo "Error deleting record: ";
+}
+
+?>
+</body>
+</html>
